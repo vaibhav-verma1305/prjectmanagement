844403997b99ab5bf89fa9a90aeec5b1abe08b47
deepanshu1909 <113288035+deepanshu1909@users.noreply.github.com>
2023-04-12 21:46:43 +0530

    Delete Backend directory

--- Backend/gu/gupms/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from gupms.models import Team
-from gupms.models import Account
-
-# Register your models here.
-admin.site.register(Team)
-admin.site.register(Account)
